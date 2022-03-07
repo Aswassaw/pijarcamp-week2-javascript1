a51077f5d5f6603fa48d4b560e4498db2d04f004
Andry Pebrianto <andrypeb227@gmail.com>
2022-03-07 16:13:26 +0700

    Memperbarui task2, mengubah dari fungsi menjadi prosedural

--- task2.js
+++ task2.js
@@ -1,47 +1,50 @@
-const countAverage = ({ math, indonesian, english, science }) => {
-  // validasi nilai harus diisi
-  if (math && indonesian && english && science) {
-    // validasi tipe data harus number
-    if (
-      typeof math === "number" &&
-      typeof indonesian === "number" &&
-      typeof english === "number" &&
-      typeof science === "number"
-    ) {
-      // validasi validitas nilai
-      if (
-        math <= 100 &&
-        math >= 0 &&
-        indonesian <= 100 &&
-        indonesian >= 0 &&
-        english <= 100 &&
-        english >= 0 &&
-        science <= 100 &&
-        science >= 0
-      ) {
-        const average = (math + indonesian + english + science) / 4;
+const mtk = 80,
+  bahasaIndonesia = 90,
+  bahasaInggris = 89,
+  ipa = 69;
 
-        console.log(`Rata-rata = ${average}`);
-        if (average >= 90) {
-          console.log("Grade = A");
-        } else if (average >= 80 && average < 90) {
-          console.log("Grade = B");
-        } else if (average >= 70 && average < 80) {
-          console.log("Grade = C");
-        } else if (average >= 60 && average < 70) {
-          console.log("Grade = D");
-        } else if (average >= 0 && average < 60) {
-          console.log("Grade = E");
-        }
-      } else {
-        console.log("Semua nilai yang diinput tidak boleh lebih dari 100 dan kurang dari 0!");
+// validasi nilai harus diisi
+if (mtk && bahasaIndonesia && bahasaInggris && ipa) {
+  // validasi tipe data harus number
+  if (
+    typeof mtk === "number" &&
+    typeof bahasaIndonesia === "number" &&
+    typeof bahasaInggris === "number" &&
+    typeof ipa === "number"
+  ) {
+    // validasi validitas nilai
+    if (
+      mtk <= 100 &&
+      mtk >= 0 &&
+      bahasaIndonesia <= 100 &&
+      bahasaIndonesia >= 0 &&
+      bahasaInggris <= 100 &&
+      bahasaInggris >= 0 &&
+      ipa <= 100 &&
+      ipa >= 0
+    ) {
+      const average = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
+
+      console.log(`Rata-rata = ${average}`);
+      if (average >= 90) {
+        console.log("Grade = A");
+      } else if (average >= 80 && average < 90) {
+        console.log("Grade = B");
+      } else if (average >= 70 && average < 80) {
+        console.log("Grade = C");
+      } else if (average >= 60 && average < 70) {
+        console.log("Grade = D");
+      } else if (average >= 0 && average < 60) {
+        console.log("Grade = E");
       }
     } else {
-      console.log("Semua nilai yang diinput harus bertipe number!");
+      console.log(
+        "Semua nilai yang diinput tidak boleh lebih dari 100 dan kurang dari 0!"
+      );
     }
   } else {
-    console.log("Semua nilai mata pelajaran harus diinput!");
+    console.log("Semua nilai yang diinput harus bertipe number!");
   }
-};
-
-countAverage({ math: 80, indonesian: 90, english: 89, science: 69 });
+} else {
+  console.log("Semua nilai mata pelajaran harus diinput!");
+}
